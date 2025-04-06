1e801dfc7194a4e39159e534b2aa0adfc318f0ff
rajkumar.metti <rajkumar.metti@servicenow.com>
2025-04-06 19:36:46 +0530

    ng for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { BeigeComponent } from "./beige/beige.component";
 import { CourseCardComponent } from './course-card/course-card.component';
 import { db } from "./db";
 import { DB } from './Data';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BeigeComponent, CourseCardComponent],
+  imports: [RouterOutlet, BeigeComponent, CourseCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
